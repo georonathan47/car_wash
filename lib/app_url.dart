e8c2c96d14a72967b4f8900333d0a660bdae34bd
Napollo-Software <62165761+emazeem@users.noreply.github.com>
2023-09-14 18:30:14 +0500

    Create app_url.dart

---
+++ lib/app_url.dart
@@ -0,0 +1,30 @@
+class AppUrl {
+  static var url = 'https://beclean-sa.bio/';
+  static var baseUrl = url + 'api/';
+
+  static var login = baseUrl + 'login';
+  static var register = baseUrl + 'register';
+  static var createCarSubscription = baseUrl + 'createCarSubscription';
+  static var cancelSubscription = baseUrl + 'cancelSubscription';
+  static var fetchCustomers = baseUrl + 'fetchCustomers';
+  static var fetchSubscriptions = baseUrl + 'fetchSubscriptions';
+  static var fetchInvoices = baseUrl + 'fetchInvoices';
+  static var fetchCars= baseUrl + 'fetchCars';
+  static var fetchUser= baseUrl + 'fetchUser';
+  static var editUser= baseUrl + 'editUser';
+  static var createExpense= baseUrl + 'createExpense';
+  static var fetchTasks= baseUrl + 'fetchTasks';
+  static var fetchMyCars= baseUrl + 'fetchMyCars';
+  static var fetchTask= baseUrl + 'fetchTask';
+  static var updateUser = baseUrl + 'updateUser';
+  static var paymentMarkAsDone = baseUrl + 'paymentMarkAsDone';
+  static var taskMarkAsDone = baseUrl + 'taskMarkAsDone';
+  static var changePassword = baseUrl + 'changePassword';
+  static var fetchActivities = baseUrl + 'fetchActivities';
+  static var fetchExpenses = baseUrl + 'fetchExpenses';
+  static var fetchMyExpenses = baseUrl + 'fetchMyExpenses';
+  static var fetchMyTransactions = baseUrl + 'fetchMyTransactions';
+  static var uploadTaskImage = baseUrl + 'uploadTaskImage';
+  static var uploadReceipt = baseUrl + 'uploadReceipt';
+  static var checkout = baseUrl + 'checkout';
+}
